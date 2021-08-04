052312ca920e2750922fade3d79145c8123b9c35
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 17:35:42 +0300

    useFakeProcessing hook re-exported in /src/hooks/index.js file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import useAPI from "./useAPI";
+import useFakeProcessing from "./useFakeProcessing";
 import useNavigateToPage from "./useNavigateToPage";
 import useWindowDimensions from "./useWindowDimensions";
 
-export { useAPI, useNavigateToPage, useWindowDimensions };
+export { useAPI, useFakeProcessing, useNavigateToPage, useWindowDimensions };
