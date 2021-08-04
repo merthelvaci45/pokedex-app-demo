e9b761c6afefd643cf58a958936a059506b3df78
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 08:08:06 +0300

    all utility files re-exported in /src/utils/index.js file

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+import { API_BASE_URL, API_METHODS, PAGE_ROUTES } from "./constants";
+import capitalizeFirstLetter from "./capitalize_first_letter";
+import menuItems from "./menu_items";
+import splitUrl from "./split_url";
+
+export {
+  API_BASE_URL,
+  API_METHODS,
+  PAGE_ROUTES,
+  capitalizeFirstLetter,
+  menuItems,
+  splitUrl,
+};
