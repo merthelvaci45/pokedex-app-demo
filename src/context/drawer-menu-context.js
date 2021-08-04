4a9334ccaa8e5e5f5183ba5de8ae20bc93cb2fd1
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 08:11:00 +0300

    drawer-menu-context file created in /src/context directory

---
+++ src/context/drawer-menu-context.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const DrawerMenuContext = createContext(undefined);
+
+export const DrawerMenuProvider = ({ children }) => {
+  const [isDrawerMenuOpen, setIsDrawerMenuOpen] = useState(false);
+
+  /**
+   * this handler function is responsible for toggling drawer menu upon clicking drawer menu button for mobile devices
+   */
+  const toggleDrawerMenuDisplayHandler = () =>
+    setIsDrawerMenuOpen((prevState) => !prevState);
+
+  const contextValue = {
+    isDrawerMenuOpen,
+    toggleDrawerMenu: toggleDrawerMenuDisplayHandler,
+  };
+
+  return (
+    <DrawerMenuContext.Provider value={contextValue}>
+      {children}{" "}
+    </DrawerMenuContext.Provider>
+  );
+};
