4ec7ad1484d41811fd1ebbf757c59380a8bb76fa
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 08:05:56 +0300

    app-wide constants created in /src/utils/constants.js file

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const API_BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+
+export const API_METHODS = {
+  GET: "get",
+  POST: "post",
+  PUT: "put",
+  DELETE: "delete",
+  PATCH: "patch",
+};
+
+export const PAGE_ROUTES = {
+  home: "/",
+  caughtPokemons: "/caught-pokemons",
+  favoritePokemons: "/favorite-pokemons",
+};
