95b7fd422a60a3924f9b5440493ec100973d0f98
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 22:39:59 +0300

    new classname added conditionally in Spinner.jsx file

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,27 @@
+import PropTypes from "prop-types";
+
 import classes from "./Spinner.module.scss";
 
 /**
  * this component display a loading spinner whenever any data in any page/component
  * is being loaded.
  */
-const Spinner = () => {
-  return <div className={classes.Spinner}></div>;
+const Spinner = ({ isLargeSpinner }) => {
+  return (
+    <div
+      className={`${classes.Spinner} ${
+        isLargeSpinner ? classes.LargeSpinner : classes.SmallSpinner
+      }`}
+    ></div>
+  );
+};
+
+Spinner.propTypes = {
+  isLargeSpinner: PropTypes.bool,
+};
+
+Spinner.defaultProps = {
+  isLargeSpinner: false,
 };
 
 export default Spinner;
