67696b90f5054b4730082b239d488680b85e2b57
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 20:43:06 +0300

    unused useWindowDimensions hook removed and return JSX updated in Layout.jsx file

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -8,29 +8,18 @@ import MobileDrawerMenu from "../MobileDrawerMenu";
 
 import { DrawerMenuProvider } from "../../context";
 
-import { useWindowDimensions } from "../../hooks";
-
 /**
  * this component is a wrapper for any page component
  * and it defines the general layout for each page.
  */
 const Layout = ({ children }) => {
-  const { width } = useWindowDimensions();
-
-  const layoutContent =
-    width < 768 ? (
+  return (
+    <>
       <DrawerMenuProvider>
         <Header />
         <Backdrop />
         <MobileDrawerMenu />
       </DrawerMenuProvider>
-    ) : (
-      <Header />
-    );
-
-  return (
-    <>
-      {layoutContent}
       <main className={classes.Layout}>{children}</main>
     </>
   );
