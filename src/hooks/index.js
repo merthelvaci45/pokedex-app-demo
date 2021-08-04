c26b5edba8c657a8ca7af1e672f9ce56c3caa560
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-04 08:05:04 +0300

    all custom hooks re-exported in /src/hooks/index.js file

---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+import useAPI from "./useAPI";
+import useNavigateToPage from "./useNavigateToPage";
+import useWindowDimensions from "./useWindowDimensions";
+
+export { useAPI, useNavigateToPage, useWindowDimensions };
